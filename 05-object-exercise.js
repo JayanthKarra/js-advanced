92e2c8da18e6b74e47a791669fe6471066f25007
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-21 10:20:01 +0530

    done

---
+++ 05-object-exercise.js
@@ -0,0 +1,12 @@
+book = {};
+
+book.title = 'Crime and Punishment';
+book.pages = 1240;
+book.price = 900.2;
+
+book.author = {};
+book.author.name = 'Fyodor Dostoevsky';
+book.author.country = 'Russia';
+book.author.genre = 'novels';
+
+console.log(book);
