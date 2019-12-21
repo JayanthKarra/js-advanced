18ba30f459c056bc229a6500f7c19033572d35cd
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-21 09:53:44 +0530

    done

---
+++ 03-equality.js
@@ -0,0 +1,16 @@
+a = 100;
+b = '100';
+
+// normal equality: types do not matter
+if (a == b) {
+  console.log('Normal equality');
+}
+
+// strict equality: types matter
+if (a === b) {
+  console.log('Strict equality');
+}
+
+//  Please check these:
+//  !=
+//  !==
