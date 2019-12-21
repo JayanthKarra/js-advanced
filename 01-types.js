ca18e84b38645c275334f2b6404551101bdc154a
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-21 09:30:12 +0530

    init

---
+++ 01-types.js
@@ -0,0 +1,10 @@
+// Types are there, but a variable can keep on changing the types
+
+// number is a double precision floating point unit (64 bits)
+i = 0;
+
+console.log(typeof i);
+
+i = 1.2587888;
+
+console.log(typeof i);
